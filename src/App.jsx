aa43d7bc180a0dfd6555b9f4b32d687120ccdefd
Nabil-Teamslackpull <135339987+Teamslackpull@users.noreply.github.com>
2024-07-17 08:23:11 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+import MemePage from '/src/Components/MemePage.jsx'
+import Header from "/src/Components/Header.jsx"
+import './App.css'
+
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+           <MemePage />
+        </div>
+    )
+}
+
